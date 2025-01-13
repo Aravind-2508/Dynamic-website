68bcc2da6d39552bd90f6a6033a02b11754e8840
Aravind-2508 <149137311+Aravind-2508@users.noreply.github.com>
2025-01-13 14:07:17 +0000

    javascript

---
+++ cat_project/cat.js
@@ -0,0 +1,15 @@
+function offSwitch(){
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switch Off";
+    document.getElementById("offSwitched").style.backgroundColor="grey";
+    document.getElementById("onSwitched").style.backgroundColor="green";
+
+}
+function onSwitch(){
+    document.getElementById("bulbImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "Switch On";
+    document.getElementById("offSwitched").style.backgroundColor="red";
+    document.getElementById("onSwitched").style.backgroundColor="grey";
+}
